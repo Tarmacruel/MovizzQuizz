ad22f6de4ddd7a4da67497e173c8d036f89554f6
Tarmacruel <jonatassousa@outlook.com>
2026-04-29 12:28:56 -0300

    fix(server): import question bank from js module

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
 import { customAlphabet } from "nanoid";
-import questions from "../data/questions.json" assert { type: "json" };
+import questions from "../data/questions.js";
 import {
   createRoom,
   getPublicRoom,
